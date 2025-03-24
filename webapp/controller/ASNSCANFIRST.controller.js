fa3d04cee41acfb0fc25fcd00b4940f93da77d86
VKalyanii <kalyanif0004@gmail.com>
2025-03-24 11:54:43 +0530

    24-03-25

--- webapp/controller/ASNSCANFIRST.controller.js
+++ webapp/controller/ASNSCANFIRST.controller.js
@@ -1,12 +1,48 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller,MessageToast) => {
     "use strict";
 
     return Controller.extend("ns.rfapp.controller.ASNSCANFIRST", {
         onInit() {
         },
-         // Setup router to handle navigation
+        // ASn Scanned
+        onASNScannedInFirstScreen:async function (){
+            var oModel = this.getOwnerComponent().getModel(); // Get the OData model for making requests
+            var that = this; // Preserve the controller context for the success handler
+            let oASN = this.getView().byId("idASNInputIn_ASNSCANFIRST").getValue();
+           
+          
+            if (oASN.length === 10) { // Proceed if the ASN value is not empty
+              
+     
+                // Call OData service to validate if the ASN exists in the system
+             
+                   await oModel.read(`/ZEWM_S_RHUBYASN_setSet('${oASN}')`, {
+                        success: function (odata) {
+                            console.log(odata)
+                            if (odata.ASN === oASN) {
+     
+                                MessageToast.show("Succesfully Scanned");
+                                var oRouter = that.getOwnerComponent().getRouter();
+                               oRouter.navTo("StandardSecond");
+                                // If HU exists, toggle screens and populate input fields with the retrieved data
+               
+                // if (oEvent === this.getView().byId("idASNInputIn_ASNSCANFIRST").getValue()) {
+                //     var oASNValue = oEvent
+                            }    
+                        },
+                        error: function (oError) {
+                            // Handle error if ASN not found
+                            MessageToast.show("Please Enter correct ASN")
+                        }
+                       
+               
+                })
+            }
+            },
+        // Setup router to handle navigation
          
  
         OnPressBackIn_ASNSCANFirst:function (){
@@ -21,13 +57,13 @@ sap.ui.define([
             oRouter.navTo("StandardSecond"); 
 
         },
-        onASNScannedInFirstScreen:function(){
-            var olength = this.getView().byId("idASNInputIn_ASNSCANFIRST").getValue().length;
-           if(olength){
-            var oRouter = this.getOwnerComponent().getRouter(this);
-            oRouter.navTo("StandardSecond"); 
-           }
-        }
+        // onASNScannedInFirstScreen:function(){
+        //     var olength = this.getView().byId("idASNInputIn_ASNSCANFIRST").getValue().length;
+        //    if(olength){
+        //     var oRouter = this.getOwnerComponent().getRouter(this);
+        //     oRouter.navTo("StandardSecond"); 
+        //    }
+        // }
 
     });
 });
